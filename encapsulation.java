de6bf9d83b39622d8d791c66f8e063ce9e0a6300
Charan02-r <kirankumar2004rb@gmail.com>
2025-08-29 03:36:45 -0700

    Add files via upload

---
+++ encapsulation.java
@@ -0,0 +1,11 @@
+package com.tpointtech;    
+class Main {    
+public static void main(String[] args) {    
+   
+Student s=new Student();    
+    
+s.setName("vijay");    
+    
+System.out.println(s.getName());    
+}    
+}       

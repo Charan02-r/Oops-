5196da462e6a5d7edfc009a15b5b814d323ba37a
Charan02-r <kirankumar2004rb@gmail.com>
2025-09-01 03:29:20 -0700

    Add files via upload

---
+++ string.java
@@ -0,0 +1,10 @@
+class Main{
+  public static void main(String[] args){
+    String first = "school";
+    String second = "college";
+    String third = "job";
+    System.out.println(first);
+    System.out.println(second);
+    System.out.println(third);
+  }
+}

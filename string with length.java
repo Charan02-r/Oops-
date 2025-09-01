e9ad48647c96025fa7a36b4269fd078a68065b12
Charan02-r <kirankumar2004rb@gmail.com>
2025-09-01 03:22:25 -0700

    Add files via upload

---
+++ string with length.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+
+    String greet = "R B CHARAN";
+    System.out.println("String: " + greet);
+
+    int length = greet.length();
+    System.out.println("Length: " + length);
+  }
+}

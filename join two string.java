991b564f96d86d168e6b807c332cd981b1b95940
Charan02-r <kirankumar2004rb@gmail.com>
2025-09-01 03:31:37 -0700

    Add files via upload

---
+++ join two string.java
@@ -0,0 +1,14 @@
+class Main{
+ 
+  public static void main(String[] args){
+    String first = "cyber";
+    System.out.println("first String:"+first);
+    
+    String second = "security";
+    System.out.println("second String:"+second);
+    
+    String joinedString = first.concat(second);
+    System.out.println("joined String:"+ joinedString);
+
+ }
+}

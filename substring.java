991268ba1706eddc8a9163c912fc66f6d13cc1a2
Charan02-r <kirankumar2004rb@gmail.com>
2025-09-02 03:39:25 -0700

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    String str1 = "CHARAN";
+     String str2 = "RB";
+    
+    System.out.println(str1.substring(0, 4));
+    
+    System.out.println(str2.substring(0,2));
+    
+  }
+}

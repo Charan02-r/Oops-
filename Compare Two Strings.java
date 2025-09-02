008b5792f2ea71dbf012a15d5a23b26cb461cab0
Charan02-r <kirankumar2004rb@gmail.com>
2025-09-02 03:38:47 -0700

    Add files via upload

---
+++ Compare Two Strings.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args){
+
+    String first = "java";
+    String second = "java";
+    String third = "python";
+
+    Boolean result1 = first.equals(second);
+    System.out.println("Strings first and second are equal: " + result1);
+
+    Boolean result2 = first.equals(third);
+    System.out.println("Strings first and third are equal: " + result2);
+  }
+}

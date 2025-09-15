468d42e09374b652b8b7ad9ffadce37585ca11d5
Charan02-r <kirankumar2004rb@gmail.com>
2025-09-15 03:14:10 -0700

    Add files via upload

---
+++ array.java
@@ -0,0 +1,11 @@
+public class Main  {     
+
+public static void main(String[] args)   {     
+
+ int[] num = new int[50];     
+  
+int arrayLength=num.length;     
+
+System.out.println("The length of the array is: "+ arrayLength);     
+}     
+}    

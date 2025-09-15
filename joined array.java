223ce1405341328031452b4bf9caed42c3e292ae
Charan02-r <kirankumar2004rb@gmail.com>
2025-09-15 03:14:25 -0700

    Add files via upload

---
+++ joined array.java
@@ -0,0 +1,10 @@
+public class Main   {     
+public static void main(String[] args)   {     
+   
+String[] country = { "India", "Australia", "Japan", "USA", "UAE", "Canada", "Brazil","uk", "italy","china","Russia"};    
+    
+int arrayLength=country.length; 
+
+System.out.println("The size of the array is: " + arrayLength);     
+}     
+}    

d11b6f0ce05b5d3691c25180396512649761f073
Charan02-r <kirankumar2004rb@gmail.com>
2025-09-25 03:04:06 -0700

    Add files via upload

---
+++ overflow.java
@@ -0,0 +1,15 @@
+public class Main{
+    
+    public static void main (String[]args){
+        
+        double a=8.328492534;  
+     
+     byte b=(byte)a;  
+     
+     System.out.println(a);  
+     System.out.println(b);  
+     
+     
+     
+    }
+}

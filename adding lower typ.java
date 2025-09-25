29186f8f2998796bd5fc335b6e1d5e7792afacbd
Charan02-r <kirankumar2004rb@gmail.com>
2025-09-25 03:02:27 -0700

    Add files via upload

---
+++ adding lower typ.java
@@ -0,0 +1,11 @@
+   public class Main{
+       public static void main(String[]args){
+          
+          byte a=40;
+          byte b=50;
+          
+          byte  c=(byte)(a+b);
+          System.out.println(c);
+           
+       }
+   }
